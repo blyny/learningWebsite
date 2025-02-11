298cc5bd0a2d0279b6280feded04062091383dc0
blyny <126427702+blyny@users.noreply.github.com>
2025-02-11 15:34:42 -0500

    Add files via upload

---
+++ new-script.js
@@ -0,0 +1,12 @@
+function go_to_project() {
+    window.location.href = "projects.html";
+}
+
+function go_to_about_me() {
+    window.location.href = "about-me.html";
+
+}
+
+function go_to_resume() {
+    window.location.href = "main_resume.pdf";
+}
